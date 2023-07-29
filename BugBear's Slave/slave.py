d577caeb26108a4ec1f24fddc35b8f6a3a4f39c1
Davide Altamura <davide.altamura97@gmail.com>
2023-07-29 09:45:23 +0200

    Solve #8
    Changed endpoint, added snapshot date to calculate requirements based on snapshot power instead of current one

--- BugBear's Slave/slave.py
+++ BugBear's Slave/slave.py
@@ -11,11 +11,10 @@ from StringProgressBar import progressBar
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('GUILD_ID')
-ROKSTATS_API = os.getenv('ROKSTATS_API')
-TODAY_DATE = today = date.today()
+ROKSTATS_API = os.getenv('ROKSTATS_APY')
 
 intents = discord.Intents.default()
-intents.message_content = True
+intents.message_content = True 
 bot = commands.Bot(command_prefix="/", intents=intents)
 objDiscordGuildID = discord.Object(id=int(GUILD))
 
@@ -23,7 +22,10 @@ discord_db = DiscordDB()
 requirements = Requirements()
 bot.remove_command("help")
 
-# This function assigns players a status to determine rewards - Kingdom policy
+TODAY_DATE = today = date.today()
+START_DATE = "2023-07-23"
+SNAPSHOT_DATE = "2023-06-28"
+
 async def find_status(kills_percentage, deads_percentage):
     if kills_percentage >= 100 and deads_percentage >= 100:
         return "MGE Competitor"
@@ -41,25 +43,28 @@ async def find_status(kills_percentage, deads_percentage):
         return "Unknown"
 
 async def check_id(gov_id: int):
-    url = f"https://rokstats.online/api/governors/summary?apiKey={ROKSTATS_API}&searchGovernor={gov_id}&startDate=2023-06-28&endDate={TODAY_DATE}"
+    url = f"https://rokstats.online/api/governor/{gov_id}/summary?apiKey={ROKSTATS_API}&startDate={START_DATE}&endDate={TODAY_DATE}&initialSnapshotDate={SNAPSHOT_DATE}"
     response = requests.get(url)
     data = response.json()
     return data if data else False
 
 async def send_id_stats(response, author_id, interaction: discord.Interaction=None, channel=None):
-    last_scan = response[0]['SnapshotTime']
+ 
+    last_scan = response['SnapshotTime']
     date_only = last_scan.split('T')[0]
-    gov_id = response[0]['GovernorId']
-    name = response[0]['Name']
-    alliance = response[0]['Alliance']
-    kp = response[0]["KillPoints"]["Total"]
-    totdeads = response[0]["Dead"]
-    power = response[0]['Power']
-    CurrentT4 = response[0]["killPointsDiff"]["T4"]
-    CurrentT5 = response[0]["killPointsDiff"]["T5"]
-    CurrentDeads = response[0]["deadDiff"]
+    gov_id = response['GovernorId']
+    name = response['Name']
+    alliance = response['Alliance']
+    kp = response["KillPoints"]["Total"]
+    totdeads = response["Dead"]
+    power = response['Power']
+    initial_snapshot_data = response['initialSnapshot']
+    power_initial_snapshot = initial_snapshot_data.get('Power')
+    CurrentT4 = response["killPointsDiff"]["T4"]
+    CurrentT5 = response["killPointsDiff"]["T5"]
+    CurrentDeads = response["deadDiff"]
     CurrentKills = int(CurrentT4) + int(CurrentT5)
-    kill_req, deads_req = requirements.find_requirements(power)
+    kill_req, deads_req = requirements.find_requirements(power_initial_snapshot)
 
     kills_percentage = CurrentKills * 100 // int(kill_req)
     deads_percentage = CurrentDeads * 100 // int(deads_req)
@@ -69,26 +74,20 @@ async def send_id_stats(response, author_id, interaction: discord.Interaction=No
     size = 15
     killsbar = progressBar.filledBar(total, kills_percentage, size)
     deadsbar = progressBar.filledBar(total, int(deads_percentage), size)
-
-    user = await bot.fetch_user(author_id)
-    if user.avatar:
-        userpfp = user.avatar
-    else:
-        userpfp = "https://media.discordapp.net/attachments/1076154233197445201/1127610236744773792/discord-black-icon-1.png"
-
+    
     if response:
         embed = discord.Embed(color=0x00ffe5)
         embed.title = f"KvK Personal stats"
-        embed.set_thumbnail(url=f"{userpfp}")
-        description = f"Governor: {name if name else '0'}\nPower snapshot: {power if power else '0'}\nGovernor ID: {gov_id if gov_id else '0'}\nAlliance: {alliance if alliance else '0'}\nAccount KP: {kp if kp else '0'}\nAccount deads: {totdeads if totdeads else '0'}\nAccount status: {player_status if player_status else '0'}\n"
+        embed.set_thumbnail(url=f"https://rokstats.online/img/governors/{gov_id}.jpg")
+        description = f"Governor: {name if name else '0'}\nPower snapshot: {power_initial_snapshot if power_initial_snapshot else '0'}\nCurrent power: {power if power else '0'}\nGovernor ID: {gov_id if gov_id else '0'}\nAlliance: {alliance if alliance else '0'}\nAccount KP: {kp if kp else '0'}\nAccount deads: {totdeads if totdeads else '0'}\nAccount status: {player_status if player_status else '0'}\n"
 
         embed.description = description
 
         embed.add_field(name="KILLS REQUIRED", value=kill_req, inline=True)
         embed.add_field(name="DEADS REQUIRED", value=deads_req, inline=True)
 
-        embed.add_field(name="CURRENT KILLS", value=f"{CurrentKills}\n{killsbar[0]}   {killsbar[1]}%", inline=False)
-        embed.add_field(name="CURRENT DEADS", value=f"{CurrentDeads}\n{deadsbar[0]}   {deadsbar[1]}%", inline=False)
+        embed.add_field(name="CURRENT KILLS", value=f"{CurrentKills}\n{killsbar[0]}   {int(killsbar[1])}%", inline=False)
+        embed.add_field(name="CURRENT DEADS", value=f"{CurrentDeads}\n{deadsbar[0]}   {int(deadsbar[1])}%", inline=False)
 
         embed.set_footer(text=f"Scan date: {date_only}\n\nData from www.rokstats.online made by @ibugbear\nBot by @themaxi7")
 
@@ -116,7 +115,7 @@ async def on_message(msg: discord.Message):
         if request == "stats":
             id_from_db = discord_db.get_id_from_discord(author_id=author_id)
             if id_from_db:
-               url=f"https://rokstats.online/api/governors/summary?apiKey={ROKSTATS_API}&searchGovernor={id_from_db}&startDate=2023-06-28&endDate={TODAY_DATE}"
+               url=f"https://rokstats.online/api/governor/{id_from_db}/summary?apiKey={ROKSTATS_API}&startDate={START_DATE}&endDate={TODAY_DATE}&initialSnapshotDate={SNAPSHOT_DATE}"
                response = requests.get(url)
                await send_id_stats(response.json(), channel=channel, author_id=int(author_id))
             else:
@@ -141,5 +140,4 @@ async def main():
     await bot.start(TOKEN, reconnect=True)
 
 asyncio.run(main())
-
 
